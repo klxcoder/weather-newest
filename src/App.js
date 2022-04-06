2219f47cf3ee1a8f6284878e68d97933b798e0c3
klxcoder <klxcoder@gmail.com>
2022-04-06 12:08:53 +0700

    fixed Home component props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import Details from './components/weatherByDay/WeatherByDay';
 import Home from './components/home/Home';
 
+import {
+  hour,
+  minute,
+  year,
+  amPm,
+  date,
+  day,
+  month,
+  toDay,
+} from './contansts/contansts';
+
 function App() {
   return (
     <div className="App">
       <div className="wapperAppWeather">
-        <Home />
+        <Home hour={hour} minute={minute} year={year} amPm={amPm} date={date} day={day} month={month} toDay={toDay} />
         <Details />
       </div>
     </div>
